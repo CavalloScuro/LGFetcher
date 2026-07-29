020ab5fbffdd3abb622d81b971792748d1d6254f
CavalloScuro <79542118+CavalloScuro@users.noreply.github.com>
2026-07-29 11:37:30 -0700

    Add files via upload

---
+++ prefs.js
@@ -0,0 +1,5 @@
+pref("extensions.lgfetcher.mirrors", "https://libgen.li\\nhttps://libgen.la\\nhttps://libgen.gl\\nhttps://libgen.bz\\nhttps://libgen.vg\\nhttps://libgen.is");
+pref("extensions.lgfetcher.formats.pdf", true);
+pref("extensions.lgfetcher.formats.epub", true);
+pref("extensions.lgfetcher.formats.djvu", true);
+pref("extensions.lgfetcher.formats.mobi", true);
